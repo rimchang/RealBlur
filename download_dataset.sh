5c90ed544b0b23ca613f7b57f9e7a24bba4c574e
jsrim <jsrim@dgist.ac.kr>
2020-08-18 14:02:05 +0900

    train file

---
+++ download_dataset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget http://cg.postech.ac.kr/research/RealBlur/RealBlur.zip # RealBlur
+wget http://cg.postech.ac.kr/research/RealBlur/BSD_B_Centroid.zip # BSD-B
+wget https://cv.snu.ac.kr/~snah/Deblur/dataset/GOPRO_Large.zip # GoPro
+
+
